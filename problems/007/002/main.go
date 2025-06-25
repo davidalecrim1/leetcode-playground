d70356398c7e5172c8b81825fa31d1072f7b94f2
David Alecrim <dsalecrim@outlook.com>
2025-06-25 07:51:08 -0300

    reimplement reverse function in two separate files for improved organization

---
+++ problems/007/002/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+func reverse(x int) int {
+	sign := 1
+	if x < 0 {
+		sign = -1
+	}
+	y := 0
+	x = int(math.Abs(float64(x)))
+
+	for x != 0 {
+		// 123
+		mod := x % 10
+		x /= 10
+
+		if y > (math.MaxInt32-mod)/10 {
+			return 0
+		}
+
+		y = y*10 + mod
+	}
+
+	return y * sign
+}
