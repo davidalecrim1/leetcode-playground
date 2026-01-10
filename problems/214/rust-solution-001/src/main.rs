ac29772126d9ef36781fac95c27cbe508832db44
David Alecrim <dsalecrim@outlook.com>
2026-01-10 14:27:55 -0300

    feat: add new problems

---
+++ problems/214/rust-solution-001/src/main.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s1: Vec<char> = s.chars().collect();
+        s1.sort_unstable();
+
+        let mut t1: Vec<char> = t.chars().collect();
+        t1.sort_unstable();
+
+        s1 == t1
+    }
+}
