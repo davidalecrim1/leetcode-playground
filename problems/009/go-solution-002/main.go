7fac8caf162708d3bb7b11b6f2fedff67cfc990e
David Alecrim <dsalecrim@outlook.com>
2026-05-02 14:33:24 -0300

    refactor(009): move go solutions into dedicated folders

---
+++ problems/009/go-solution-002/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "strings"
+
+func isPalindrome(s string) bool {
+	left, right := 0, len(s)-1
+
+	for left < right {
+		if !isAlphanumeric(s[left]) {
+			left++
+			continue
+		}
+
+		if !isAlphanumeric(s[right]) {
+			right--
+			continue
+		}
+
+		if strings.ToLower(string(s[left])) != strings.ToLower(string(s[right])) {
+			return false
+		}
+
+		left++
+		right--
+	}
+
+	return true
+}
+
+func isAlphanumeric(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
+}
